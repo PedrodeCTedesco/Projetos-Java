152ee2eac90533083b0da5437056d01052101639
Pedro de Castro Tedesco <pedrocastro.tedesco@gmail.com>
2024-01-25 18:07:47 -0300

    feat (new exercise: done): Rational class with basic arithmetic operations

--- Classes e Objetos/RationalNumbers/Rational.java
+++ Classes e Objetos/RationalNumbers/Rational.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 /** Crie uma classe chamada Rational para realizar aritmética com frações. Escreva um programa para testar sua classe. 
  * Use variáveis de inteiros para representar as variáveis de instância private da classe — o numerator e o denominator.
  * Forneça um construtor que permita que um objeto dessa classe seja inicializado quando ele for declarado. O construtor deve armazenar a fração em uma forma reduzida.
@@ -34,6 +35,7 @@ public class Rational
 		this.denominator = denominator / calculateMDC(numerator, denominator);
 	};//fim do construtor
 	
+	//Métodos cliente
 	
 	public int getNumerator()
 	{
@@ -44,7 +46,127 @@ public class Rational
 	{
 		return denominator;
 	};//fim do método getDenominator()
+
+	public static String addTwoRationals(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	{
+		//Obtendo os valores de cada fração
+		//Etapa 1: encontrar o MMC para os denominadores
+		int mmc = calculateMMC(denominator_1, denominator_2);
+		
+		//Etapa 2: converter as frações para o denominador comum
+		int numeratorRational1 = numerator_1 * (mmc / denominator_1);
+		int numeratorRational2 = numerator_2 * (mmc / denominator_2);
+		
+		//Etapa 3: somar os numeradores
+		int numeratorSum = numeratorRational1 + numeratorRational2;
+		
+		//Etapa 4: simplificar a fração resultante
+		int divisorInComum = calculateMDC(numeratorSum, mmc);
+		int simplifiedNumerator = numeratorSum / divisorInComum;
+		int simplifiedDenominator = mmc / divisorInComum;
+		
+		//Etapa 7: retornar o resultado
+		String simplifiedNumeratorString = String.valueOf(simplifiedNumerator);
+		String simplifiedDenominatorString = String.valueOf(simplifiedDenominator);
+		return String.format("%s/%s", simplifiedNumeratorString, simplifiedDenominatorString);
+	};//fim do método addTwoRationals
 	
+	public static String subtractTwoRationls(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	{
+		//Etapa 1: encontrar um denominador comum para os denominadores
+		int mmc = calculateMMC(denominator_1, denominator_2);
+		
+		//Etapa 2: multiplicar os numeradores e denominadores para obter denominadores comuns
+		int newNumerator_1 = numerator_1 * (mmc / denominator_1);
+		int newNumerator_2 = numerator_2 * (mmc / denominator_2);
+		
+		//Etapa 3: subtrair os numeradores 
+		int resultNumerator = newNumerator_1 - newNumerator_2;
+		
+		//Etapa 4: simplifique a fração, se necessário
+		int mdc = calculateMDC(resultNumerator, mmc);
+		
+		if (mdc == 1)
+			return String.format("%d/%d", resultNumerator, mmc);
+		
+		int simplifiedNumerator = resultNumerator / mdc;
+		int simplifiedDenominator = mmc / mdc;
+		
+		return String.format("%d/%d", simplifiedNumerator, simplifiedDenominator);
+	};//fim do método subtractTwoRationsl(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+
+	public static String productTwoRationals(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	{
+		//Etapa 1: multiplicar os numeradores e obter o novo numerador
+		int numeratorProduct = numerator_1 * numerator_2;
+		
+		//Etapa 2: multiplicar os denominadores e obter o novo denominador
+		int denominatorProduct = denominator_1 * denominator_2;
+		
+		//Etapa 3: deixar a fração na forma irredutível, se necessário
+		int mdc = calculateMDC(numeratorProduct, denominatorProduct);
+		
+		if(mdc == 1)
+			return String.format("%d/%d", numeratorProduct, denominatorProduct);
+		
+		int simplifiedNumeratorProduct = numeratorProduct / mdc;
+		int simplifiedDenominatorProduct = denominatorProduct / mdc;
+		
+		return String.format("%d/%d", simplifiedNumeratorProduct, simplifiedDenominatorProduct);
+			
+	};//fim do método productTwoRationals(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	
+	public static String divideTwoRationals(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	{
+		//Etapa 1: Inverter a segunda fração
+		int invertedNumerator = denominator_2;
+		int invertedDenominator = numerator_2;
+		
+		//Etapa 2: Multiplicar a primeira fração pelo inverso da segunda
+		int newNumerator = numerator_1 * invertedNumerator;
+		int newDenominator = denominator_1 * invertedDenominator;
+		
+		//Etapa 3: Retornar a fração na sua versão irredutível
+		int mdc = calculateMDC(newNumerator, newDenominator);
+		
+		if (mdc == 1)
+			return String.format("%d/%d", newNumerator, newDenominator);
+		
+		int simplifiedNumerator = newNumerator / mdc;
+		int simplifiedDenominator = newDenominator / mdc;
+		
+		return String.format("%d/%d", simplifiedNumerator, simplifiedDenominator);
+	};//fim do método divideTwoRationals(int numerator_1, int denominator_1, int numerator_2, int denominator_2)
+	
+	public String toString()
+	{
+		return String.format("Fração irredutível: %d/%d", getNumerator(), getDenominator());
+	};//fim do método toString()
+
+	public String toStringFloat()
+	{
+		//Armazenando o numerador e denominador
+		double numerator = getNumerator();
+		double denominator = getDenominator();
+		
+		//Exibindo prompt na janela de comando para o usuário especificar a quantidade de casas após a vírgula
+		Scanner input = new Scanner(System.in);
+		System.out.printf("%s%n", "Quantas casas após a vírgula você deseja na exibição do resultado?");
+		int specifier = input.nextInt();
+		
+		BigDecimal rationalResult = BigDecimal.valueOf(numerator / denominator).setScale(specifier, RoundingMode.HALF_EVEN);
+		//Gerando a saída na janela de comando
+		return String.format("The division between %.2f and %.2f is: %f", numerator, denominator, rationalResult);
+		
+	};//fim do método toStringFloat()
+
+	//Métodos implementação
+	
+	private static int calculateMMC (int number1, int number2)
+	{
+		//O MMC pode ser calculado pela divisão do produto de dois números ( a * b) e o MDC(a, b)
+		return (number1 * number2) / calculateMDC(number1, number2);
+	};//fim do método calculateMMC(int number1, int number2)
 	
 	private static int calculateMDC (int number1, int number2)
 	{
@@ -57,29 +179,5 @@ public class Rational
 		};//fim da sentença de iteração while
 		return number1;
 	};//fim do método calculateMDC(int number1, int number2)
-	
-	public String toString()
-	{
-		return String.format("Fração irredutível: %d/%d", getNumerator(), getDenominator());
-	};//fim do método toString()
-
-	public String toStringFloat()
-	{
-		//Armazenando o numerador e denominador
-		int numerator = getNumerator();
-		int denominator = getDenominator();
 		
-		//Realizando a divisão entre eles
-		float floatResult = numerator / denominator;
-		
-		//Exibindo prompt na janela de comando para o usuário especificar a quantidade de casas após a vírgula
-		Scanner input = new Scanner(System.in);
-		System.out.printf("%s%n", "Quantas casas após a vírgula você deseja na exibição do resultado?");
-		int specifier = input.nextInt();
-		
-		//Gerando a saída na janela de comando
-		return String.format("The division between %d and %d is: %f", numerator, denominator, floatResult);
-		
-	};//fim do método toStringFloat()
-	
 };//fim da classe Rational
--- Classes e Objetos/RationalNumbers/RationalTest.java
+++ Classes e Objetos/RationalNumbers/RationalTest.java
@@ -9,8 +9,14 @@ public class RationalTest
 	public static void main (String[]args)
 	{
 		//Instâncias da classe
-		Rational rational_1 = new Rational(30, 60);
+		Rational rational_1 = new Rational(3, 4);
+		Rational rational_2 = new Rational(5, 6);
 		System.out.printf("%s%n", rational_1);
-		System.out.printf("Testing toStrinfFloat(): %s", rational_1.toStringFloat());
+		System.out.printf("Testing toStrinfFloat(): %s%n", rational_1.toStringFloat());
+		
+		System.out.printf("Testing addTwoRationals(): %s%n", Rational.addTwoRationals(3, 4, 5, 6));
+		System.out.printf("Testing subtractTwoRationals(): %s%n", Rational.subtractTwoRationls(3, 4, 1, 6));
+		System.out.printf("Testing productTwoRationals(): %s%n", Rational.productTwoRationals(23, 5, 123, 41));
+		System.out.printf("Testing divdeTwoRationals(): %s%n", Rational.divideTwoRationals(8, 9, 4, 5));
 	};//fim do método main
 };//fim da classe RationalTest
