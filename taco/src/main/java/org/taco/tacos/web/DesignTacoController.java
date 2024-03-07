8a967a30121d795b75d15bd925e3565b55ce2661
Pedro de Castro Tedesco <pedrocastro.tedesco@gmail.com>
2024-03-06 21:57:34 -0300

    feat: modificado o método processTaco(...) para incluir operações de filtro entre estruturas map dos parâmetros de solicitação do formulário. Isso foi necessário para adicionarmos manualmente os objetos Ingredients no objeto Taco

--- taco/src/main/java/org/taco/tacos/web/DesignTacoController.java
+++ taco/src/main/java/org/taco/tacos/web/DesignTacoController.java
@@ -1,8 +1,11 @@
 package org.taco.tacos.web;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.taco.tacos.Ingredients;
 import org.taco.tacos.Taco;
@@ -11,12 +14,24 @@ import org.taco.tacos.TacoOrder;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 @Controller
 @RequestMapping("design")
-@SessionAttributes("tacoOrder")
+@SessionAttributes({"tacoOrder", "tacoIngredients"})
 @Slf4j
 public class DesignTacoController
 {
+    //fields
+    private final IngredientByNameConverter conversor;
+
+    //constructor
+    @Autowired
+    public DesignTacoController(IngredientByNameConverter conversor)
+    {
+        this.conversor = conversor;
+    };//fim do construtor
+
     //Model attributes
     @ModelAttribute(name = "tacoIngredients")
     public void addIngredientsToModel(Model model)
@@ -69,11 +84,45 @@ public class DesignTacoController
 
     //POST Handler
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder)
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder,
+                              HttpServletRequest request,
+                              @ModelAttribute("tacoIngredients") Map<String, Ingredients> map)
     {
+        //Filtrar os elementos de 'map' comparando com os valores recebidos de request
+        //Map com os parâmetros de solicitação. Abaixo tenho os checkbox marcados ('on')
+        //no formato Map {on = 'Flour Tortilla', on = 'Sour Cream' etc)
+        Map<String, String[]> param = request.getParameterMap();
+
+        //List de ingredientes
+        List<Ingredients> ingredients = new ArrayList<>();
+
+        //Para acessar apenas os valores presentes no array de "String" param
+        for(Map.Entry<String, String[]> entry : param.entrySet())
+        {
+            String[] values = entry.getValue();
+            for(String value : values)
+            {
+                Ingredients ingredientSelected = conversor.convert(value);
+                ingredients.add(ingredientSelected);
+            };//fim da sentença de iteração for aprimorado interna
+        };//fim da sentença de iteração for aprimorado
+
+        //Como pegamos em "param" todos os dados do formulário (inclusos dados que não são requeridos para
+        //conversão) agora nós filtramos novamente utilizando os dados adicionados ao modelo sob nome
+        //"tacoIngredients".
+        List<Ingredients> finalList = map
+                                        .values()
+                                        .stream()
+                                        .filter(ingredients::contains)
+                                        .collect(Collectors.toList());
+
+        //Configura os ingredientes no objeto Taco
+        taco.setIngredientList(finalList);
+
+        //Adiciona o objeto Taco ao objeto TacoOrder. Note que todos os campos de Taco estão inicializados
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
         return "redirect:/orders/current";
-    };//end of processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder)
+    };//end of processTaco(...)
 };//end of DesignTacoController
